d80fb670e75242381e5604711aa145a459b63feb
mariabenning <mariabenning15@gmail.com>
2021-12-05 19:18:40 -0300

    implementandofuncionalidadesdosbotoes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Home from './Pages/Home/Home';
 import Juntese from './Pages/Juntese/Juntese';
 import Receitas from './Pages/receitas/Receitas';
 import Sabermais from './Pages/Sabermais/Sabermais';
-
+import styled from "styled-components"
+import { imagenHome } from './Pages/Home/Home';
 
 
 
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,43 @@
 
 import React from 'react'
 import styled from "styled-components";
+import Illutration from '../../img/Illustration.svg';
 
+
+
+export const imagenHome = styled.img
+ `
+ width: 100%;
+ height: 100%;
+
+ `
+
+
+
+const botaoReceitas = () => {
+    console.log("apertou botão de receitas")
+
+
+}
+
+
+const botaoBlog = () => {
+
+    console.log("apertou botão de blog")
+    
+}
+
+const botaoJuntese = () => {
+
+    console.log("apertou botão juntese")
+    
+}
+
+const botaoRegistrar= () => {
+    console.log("apertou botão de registre")
+
+    
+}
 
 export default function Home() {
     return (
@@ -9,18 +45,15 @@ export default function Home() {
 
             <h1>Comida Saudável</h1>
 
-
-            <button>RECEITAS SAUDÁVEIS</button>
-            <button>BLOG</button>
-            <button>JUNTE-SE</button>
-            <button>REGISTRAR</button>
+            
+            <button onClick={botaoReceitas}>RECEITAS SAUDÁVEIS</button>
+            <button onClick={botaoBlog}>BLOG</button>
+            <button onClick={botaoJuntese}>JUNTE-SE</button>
+            <button onClick={botaoRegistrar}>REGISTRAR</button>
 
             <h2>Pronto para Tentar uma nova receita?</h2>
-            <input placeholder = "Pesquise receitas saudáveis"></input>
-
-
-
-
+            <input placeholder="Pesquise receitas saudáveis"></input>
+             <img src={Illutration} ></img> 
         </div>
     );
 }
